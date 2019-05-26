36f3720e6ee9065b39be95b43754a159935b5435
Chacko <chackop@gmail.com>
2019-05-27 00:01:41 +0100

    render json data

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,49 @@
 import React from "react";
-import DigitalClock from "../src/DigitalClock";
+import axios from "axios";
 
 class Index extends React.Component {
-  // Does not get run on client
   static async getInitialProps() {
-    // return {
-    //   time: new Date().toISOString()
-    // };
-
-    // const promise = new Promise((resolve, reject) => {
-    //   setInterval(() => {
-    //     resolve({
-    //       time: new Date().toISOString()
-    //     });
-    //   }, 3000);
-    // });
-    // return promise;
-
-    
+    var promise = axios
+      .get("http://localhost:4000/speakers")
+      .then(response => {
+        return {
+          hasErrored: false,
+          speakerData: response.data
+        };
+      })
+      .catch(error => {
+        return {
+          hasErrored: true,
+          message: error.message
+        };
+      });
+    return promise;
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      //time: new Date().toISOString()
-      time: props.time
+      hasErrored: props.hasErrored,
+      message: props.message,
+      speakerData: props.speakerData
     };
   }
 
-  tick() {
-    this.setState(() => {
-      return {
-        time: new Date().toISOString()
-      };
-    });
-  }
+  componentDidMount() {}
 
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+  componentWillUnmount() {}
 
   render() {
-    return <DigitalClock time={this.state.time} />;
+    return (
+      <ul>
+        {this.state.speakerData &&
+          this.state.speakerData.map(speaker => (
+            <li key={speaker.id}>
+              {speaker.firstName} {speaker.lastName}
+            </li>
+          ))}
+      </ul>
+    );
   }
 }
 
